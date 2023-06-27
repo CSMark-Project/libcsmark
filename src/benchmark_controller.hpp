63da9608cd430699df9f9ea58a06bdea6e6e3445
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2023-06-28 00:38:56 +0100

    add Boost's Chrono Library as a submodule

---
+++ src/benchmark_controller.hpp
@@ -0,0 +1,18 @@
+//
+//
+//
+
+#ifndef SRC_BENCHMARK_CONTROLLER_HPP
+#define SRC_BENCHMARK_CONTROLLER_HPP
+
+
+namespace csmarklib{
+    class BenchmarkController {
+
+
+
+    };
+}
+
+
+#endif //SRC_BENCHMARK_CONTROLLER_HPP
---
+++ src/benchmarks/integer_benchmark.hpp
@@ -0,0 +1,16 @@
+//
+//
+//
+
+#ifndef SRC_INTEGER_BENCHMARK_HPP
+#define SRC_INTEGER_BENCHMARK_HPP
+
+
+namespace csmarklib{
+    class IntegerBenchmark {
+
+    };
+}
+
+
+#endif //SRC_INTEGER_BENCHMARK_HPP
